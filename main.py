a87142b2ccfcbf9c92417419d30cce89395fb571
thmastin <thmastin@gmail.com>
2024-08-10 17:46:25 -0400

    Add ability to count the number of words and print

--- main.py
+++ main.py
@@ -1,10 +1,18 @@
 def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
-    print(text)
+    num_words = number_of_words(text)
+    print(f"This book contains {num_words} words.")
 
 def get_book_text(path):    
     with open(path) as f:
         return f.read()
+    
+def number_of_words(text_file):
+    words = text_file.split()
+    return len(words)  
+    
+    
+
 
 main()        
