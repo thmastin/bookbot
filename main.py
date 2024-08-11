b469f04791a84552e7791ff1ab5d3900b2c5cb0f
thmastin <thmastin@gmail.com>
2024-08-10 22:59:18 -0400

    Add final report

--- main.py
+++ main.py
@@ -3,10 +3,15 @@ def main():
     text = get_book_text(book_path)
     num_words = number_of_words(text)
     letter_count = count_of_letters(text)
+    letter_dict_list = letter_list(letter_count)
+    sorted_letter_list = sorted(letter_dict_list, key=sort_on_num, reverse=True)
     
-    print(f"This book contains {num_words} words.")
-    
-    print(sort_letters(letter_count))
+    print(f"--- Begin report of {book_path} ---")
+    print(f"Number of words: {num_words}")
+    print()
+    for letter in sorted_letter_list:
+        print(f"The '{letter['letter']}' was found {letter['num']} times")
+    print(f"---- End of report ----")    
 
 def get_book_text(path):    
     with open(path) as f:
@@ -40,14 +45,18 @@ def count_of_letters(text_file):
     
     return count_of_letters
 
-def sort_letters(dict):
+def letter_list(dict):
     list_of_count_of_letters = []
     for k in dict:
         letter_dict = {"letter": k, "num": dict.get(k)}
         print(letter_dict)
         list_of_count_of_letters.append(letter_dict)
    
-    
+    return list_of_count_of_letters
+
+def sort_on_num(d):
+    return d["num"]
+
 
 
 main()        
