dba9d4a397eb2762edd727450d8a550f4498a679
thmastin <thmastin@gmail.com>
2025-01-12 00:28:38 -0500

    Add check for character being in alphabet

--- main.py
+++ main.py
@@ -20,10 +20,11 @@ def get_num_characters(text):
     text_lower = text.lower()
     character_count = {"e": 1}
     for i in range(len(text_lower)):
-        if text_lower[i] not in character_count:
-            character_count[text_lower[i]] = 1
-        else:
-            character_count[text_lower[i]] += 1
+        if text_lower[i].isalpha() == True:        
+            if text_lower[i] not in character_count:
+                character_count[text_lower[i]] = 1
+            else:
+                character_count[text_lower[i]] += 1
     return character_count
         
 
