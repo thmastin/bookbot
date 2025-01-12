7077fd3b04f2f40d0a9a9f6436380e8dd93105da
thmastin <thmastin@gmail.com>
2025-01-11 22:01:37 -0500

    Add count of character frequency

--- main.py
+++ main.py
@@ -2,7 +2,9 @@ def main():
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
+    num_characters = get_num_characters(text)
     print(f"{num_words} words found in the document")
+    print(f"The frequency of the use of characters in the document are:{num_characters}")
 
 
 def get_num_words(text):
@@ -13,6 +15,18 @@ def get_num_words(text):
 def get_book_text(path):
     with open(path) as f:
         return f.read()
+    
+def get_num_characters(text):
+    text_lower = text.lower()
+    character_count = {"e": 1}
+    for i in range(len(text_lower)):
+        if text_lower[i] not in character_count:
+            character_count[text_lower[i]] = 1
+        else:
+            character_count[text_lower[i]] += 1
+    return character_count
+        
+
 
 
 main()
